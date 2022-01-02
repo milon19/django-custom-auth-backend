f1e35866d63088ccbed14e4d2c8ee438a47f5344
milon19 <milonmahato67@gmail.com>
2022-01-02 12:31:58 +0600

    create custom user model and setup drf

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+THIRD_PARTY_APP = [
+    'rest_framework'
+]
+
+MY_APP = [
+    'authentication.apps.AuthenticationConfig',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+] + THIRD_PARTY_APP + MY_APP
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'authentication.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
+}
